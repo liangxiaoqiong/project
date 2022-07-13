a170300367d7447ddc1434c9853356188f264ba3
liangxiaoqiong <317849807@qq.com>
2022-07-13 15:00:39 +0800

    feat(pc后台): 新增上传附件组件

--- Public/common/js/commonObj.js
+++ Public/common/js/commonObj.js
@@ -26,5 +26,43 @@ var commonObj = new Object({
     rgbStr = rgbArr.join();
     rgbStr = 'rgb' + (isOpa ? 'a' : '') + '(' + rgbStr + (isOpa ? ',' + opa : '') + ')';
     return rgbStr;
-  }
+  },
+
+  // 非空判断
+  isEmpty: function (ObjVal) {
+    if ((ObjVal == null || typeof (ObjVal) == "undefined") || (typeof (ObjVal) == "string" && ObjVal == "" && ObjVal != "undefined")) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  /*
+  //精确小数点
+  //number：为你要转换的数字
+  //digitalNum：要保留几位小数；譬如要保留2位，则值为2
+  //zerFill:是否补零。不需要补零可以不填写此参数,[true/false],默认:false
+  * */
+  digitalFloatSync: function (number,digitalNum,zeroFill) {
+    var format = typeof digitalNum !== 'undefined' ? digitalNum : 2;
+    //判断非空
+    if (!this.isEmpty(number)) {
+      //正则匹配:正整数，负整数，正浮点数，负浮点数
+      if (!/^\d+(\.\d+)?$|^-\d+(\.\d+)?$/.test(number))
+        return number;
+      var n=1;
+      for(var i=0;i<format;i++){
+        n=n*10;
+      }
+
+      //四舍五入
+      number=Math.round(number*n)/n;
+
+      //是否补零
+      if(zeroFill){
+        return number.toFixed(format);
+      }
+      return number;
+    }
+    return number;
+  },
 })
