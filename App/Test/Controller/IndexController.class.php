a9494570d04a006f03ef120ff1ab3d0de9b1bfd7
liangxiaoqiong <317849807@qq.com>
2025-05-07 14:23:07 +0800

    feat: 新增计算器组件

--- App/Test/Controller/IndexController.class.php
+++ App/Test/Controller/IndexController.class.php
@@ -9,4 +9,15 @@ class IndexController extends Controller {
         $this->display('Scroll/scroll_x');
     }
     /*endregion*/
+    /*region 计算器*/
+    public function calculator(){
+        $this->display('calculator/vue');
+    }
+    public function calculator2(){
+        $this->display('calculator/vue2');
+    }
+    public function calculator3(){
+        $this->display('calculator/vue3');
+    }
+    /*endregion*/
 }
